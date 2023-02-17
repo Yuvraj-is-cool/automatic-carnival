171f8cb5ad01f54de73f52e70a537c17558b02cc
Yuvraj-is-cool <99299241+Yuvraj-is-cool@users.noreply.github.com>
2023-02-16 16:36:23 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){
+}
+function setup(){
+    canvas=createCanvas(500,500);
+    canvas.center();
+    canvas.position(750,300);
+}
+function draw(){
+}
+function take_snapshot(){
+    save('something_even_more_special.png');
+}
